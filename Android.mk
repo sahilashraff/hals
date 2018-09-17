7e1423b9dbc5aab9e9b854cb00cb74d313e21550
Rk779 <Rahulkrishna585@gmail.com>
2018-09-17 15:28:36 +0000

    rip pathmaps
    Signed-off-by: Rk779 <Rahulkrishna585@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8952)
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 $(warning target list is : $(MSM_VIDC_TARGET_LIST))
--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -61,7 +61,7 @@ include $(CLEAR_VARS)
 # Common Includes
 libmm-vdec-inc          := $(LOCAL_PATH)/inc
 libmm-vdec-inc          += $(OMX_VIDEO_PATH)/vidc/common/inc
-libmm-vdec-inc          += $(TOP)/$(call project-path-for,qcom-media)/mm-core/inc
+libmm-vdec-inc          += $(TOP)/hardware/qcom/media-caf-msm8952/mm-core/inc
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-vdec-inc          += $(TARGET_OUT_HEADERS)/adreno
 libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/openmax
@@ -69,7 +69,7 @@ libmm-vdec-inc          += $(TOP)/frameworks/native/include/media/hardware
 libmm-vdec-inc          += frameworks/native/libs/nativewindow/include/
 libmm-vdec-inc          += frameworks/native/libs/arect/include/
 libmm-vdec-inc          += frameworks/native/libs/nativebase/include
-libmm-vdec-inc      	+= $(TOP)/$(call project-path-for,qcom-media)/libc2dcolorconvert
+libmm-vdec-inc      	+= $(TOP)/hardware/qcom/media-caf-msm8952/libc2dcolorconvert
 libmm-vdec-inc      	+= $(TOP)/frameworks/av/include/media/stagefright
 ifeq ($(call is-platform-sdk-version-at-least,27),true) #O_MR1
 libmm-vdec-inc          += $(TOP)/frameworks/native/libs/nativewindow/include
@@ -81,7 +81,7 @@ libmm-vdec-inc      	+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 ifeq ($(PLATFORM_SDK_VERSION), 18)  #JB_MR2
 libmm-vdec-def += -DANDROID_JELLYBEAN_MR2=1
-libmm-vdec-inc += $(TOP)/$(call project-path-for,qcom-media)/libstagefrighthw
+libmm-vdec-inc += $(TOP)/hardware/qcom/media-caf-msm8952/libstagefrighthw
 endif
 
 # Common Dependencies
